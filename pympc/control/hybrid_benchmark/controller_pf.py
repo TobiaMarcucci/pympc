d33e1f8ffbffc67208f4ad6ee29b5025b46c5a69
TobiaMarcucci <tobiam@mit.edu>
2018-10-04 15:17:03 -0400

    Correct handling of the initial conditions for the controller based on perspective functions.
    Correct handling of the initial conditions for the controller based on perspective functions.

--- pympc/control/hybrid_benchmark/controller_pf.py
+++ pympc/control/hybrid_benchmark/controller_pf.py
@@ -1,11 +1,13 @@
 # external imports
 import numpy as np
 import gurobipy as grb
+from scipy.linalg import block_diag
 
 # internal inputs
 from pympc.control.hybrid_benchmark.utils import (add_vars,
                                                   add_linear_inequality,
                                                   add_linear_equality,
+                                                  add_rotated_socc,
                                                   add_stage_cost,
                                                   add_terminal_cost
                                                   )
@@ -29,7 +31,7 @@ class HybridModelPredictiveController(object):
     def feedforward(self, x0):
 
         # build micp
-        prog = bild_mip_ch(x0, self.S, self.N, self.Q, self.R, self.P, self.X_N, self.norm)
+        prog = bild_mip_pf(x0, self.S, self.N, self.Q, self.R, self.P, self.X_N, self.norm)
         prog.setParam('OutputFlag', 0)
 
         # run the optimization
@@ -49,7 +51,7 @@ class HybridModelPredictiveController(object):
             sol['objective'] = prog.objVal
         return sol
 
-def bild_mip_ch(x0, S, N, Q, R, P, X_N, norm):
+def bild_mip_pf(x0, S, N, Q, R, P, X_N, norm):
 
     # shortcuts
     [nx, nu, nm] = [S.nx, S.nu, S.nm]
@@ -57,8 +59,8 @@ def bild_mip_ch(x0, S, N, Q, R, P, X_N, norm):
     # initialize program
     prog = grb.Model()
     obj = 0.
-
-    # loop over the time horizon
+    
+    # loop over time
     for t in range(N):
 
         # optimization variables
@@ -92,7 +94,7 @@ def bild_mip_ch(x0, S, N, Q, R, P, X_N, norm):
 
         # other stages
         else:
-            
+
             # constrained dynamics
             for i in range(nm):
 
@@ -105,6 +107,81 @@ def bild_mip_ch(x0, S, N, Q, R, P, X_N, norm):
                 yvi = np.concatenate((y[i], v[i]))
                 add_linear_inequality(prog, Di.A.dot(yvi), Di.b * d[i])
 
+        # infinity norm cost
+        if norm == 'inf':
+
+            # stage cost
+            su = [add_vars(prog, 1, lb=[0.])[0] for i in range(nm)]
+            obj += sum(su)
+            for i in range(nm):
+                add_linear_inequality(prog,  R.dot(v[i]), np.ones(R.shape[0]) * su[i])
+                add_linear_inequality(prog, -R.dot(v[i]), np.ones(R.shape[0]) * su[i])
+            if t == 0:
+                obj += np.max(np.abs(Q.dot(x)))
+            else:
+                sx = [add_vars(prog, 1, lb=[0.])[0] for i in range(nm)]
+                obj += sum(sx)
+                for i in range(nm):
+                    add_linear_inequality(prog,  Q.dot(y[i]), np.ones(Q.shape[0]) * sx[i])
+                    add_linear_inequality(prog, -Q.dot(y[i]), np.ones(Q.shape[0]) * sx[i])
+
+            # terminal cost
+            if t == N - 1:
+                sxN = [add_vars(prog, 1, lb=[0.])[0] for i in range(nm)]
+                obj += sum(sxN)
+                for i in range(nm):
+                    add_linear_inequality(prog,  P.dot(z[i]), np.ones(P.shape[0]) * sxN[i])
+                    add_linear_inequality(prog, -P.dot(z[i]), np.ones(P.shape[0]) * sxN[i])
+
+        # one norm cost
+        elif norm == 'one':
+
+            # stage cost
+            su = [add_vars(prog, R.shape[0], lb=[0.]*R.shape[0]) for i in range(nm)]
+            obj += sum(sum(su))
+            for i in range(nm):
+                add_linear_inequality(prog,  R.dot(v[i]), su[i])
+                add_linear_inequality(prog, -R.dot(v[i]), su[i])
+            if t == 0:
+                obj += sum(np.abs(Q.dot(x)))
+            else:
+                sx = [add_vars(prog, Q.shape[0], lb=[0.]*Q.shape[0]) for i in range(nm)]
+                obj += sum(sum(sx))
+                for i in range(nm):
+                    add_linear_inequality(prog,  Q.dot(y[i]), sx[i])
+                    add_linear_inequality(prog, -Q.dot(y[i]), sx[i])
+
+            # terminal cost
+            if t == N - 1:
+                sxN = [add_vars(prog, P.shape[0], lb=[0.]*P.shape[0]) for i in range(nm)]
+                obj += sum(sum(sxN))
+                for i in range(nm):
+                    add_linear_inequality(prog,  P.dot(z[i]), sxN[i])
+                    add_linear_inequality(prog, -P.dot(z[i]), sxN[i])
+
+        # two norm
+        elif norm == 'two':
+
+            # stage cost
+            s = [add_vars(prog, 1, lb=[0.])[0] for i in range(nm)]
+            obj += sum(s)
+            if t == 0:
+                for i in range(nm):
+                    add_rotated_socc(prog, R, v[i], s[i], d[i])
+                obj += .5*x.dot(Q.dot(x))
+            elif t < N - 1:
+                for i in range(nm):
+                    QR = block_diag(Q, R)
+                    yvi = np.concatenate((y[i], v[i]))
+                    add_rotated_socc(prog, QR, yvi, s[i], d[i])
+
+            # terminal cost
+            else:
+                for i in range(nm):
+                    QRP = block_diag(Q, R, P)
+                    yvzi = np.concatenate((y[i], v[i], z[i]))
+                    add_rotated_socc(prog, QRP, yvzi, s[i], d[i])
+
         # recompose variables
         add_linear_equality(prog, u, sum(v))
         add_linear_equality(prog, x_next, sum(z))
@@ -114,15 +191,11 @@ def bild_mip_ch(x0, S, N, Q, R, P, X_N, norm):
         # constraints on the binaries
         prog.addConstr(sum(d) == 1.)
 
-        # stage cost
-        obj += add_stage_cost(prog, Q, R, x, u, norm)
-
     # terminal constraint
     for i in range(nm):
         add_linear_inequality(prog, X_N.A.dot(z[i]), X_N.b * d[i])
 
-    # terminal cost
-    obj += add_terminal_cost(prog, P, x_next, norm)
+    # set cost
     prog.setObjective(obj)
 
     return prog
